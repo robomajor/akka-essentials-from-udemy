fa9ad9c6a47f3b7a169ce31c5f679306f0db0cbd
robomajor <Robota40%>
2022-10-25 00:31:44 +0200

    akka actors tests

---
+++ src/main/scala/part3testing/BasicSpec.scala
@@ -0,0 +1,100 @@
+package part3testing
+
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+import scala.util.Random
+
+class BasicSpec extends TestKit(ActorSystem("BasicSpec"))
+  with ImplicitSender
+  with AnyWordSpecLike
+  with BeforeAndAfterAll {
+
+  import BasicSpec._
+
+  // setup
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  "A simple actor" should {
+    "send back the same message" in {
+      val echoActor = system.actorOf(Props[SimpleActor])
+      val message = "hello test"
+      echoActor ! message
+
+      expectMsg(message) // akka.test.single-expect-default
+    }
+  }
+
+  "A blackhole actor" should {
+    "send back some message" in {
+      val blackhole = system.actorOf(Props[Blackhole])
+      val message = "hello test"
+      blackhole ! message
+
+      expectNoMessage(1 second)
+    }
+  }
+
+  "A lab test actor actor" should {
+    val labTestActor = system.actorOf(Props[LabTestActor])
+
+    "turn a string into uppercase" in {
+      labTestActor ! "I love Akka"
+      val reply = expectMsgType[String]
+      assert(reply == "I LOVE AKKA")
+    }
+    "reply to a greeting" in {
+      labTestActor ! "greeting"
+      expectMsgAnyOf("hi", "hello")
+    }
+    "reply with favorite tech" in {
+      labTestActor ! "favoriteTech"
+      expectMsgAllOf("Scala", "Akka")
+    }
+    "reply with cool tech in different way" in {
+      labTestActor ! "favoriteTech"
+      val messages = receiveN(2) // Seq[Any]
+    }
+    "reply with cool tech in a fancy way" in {
+      labTestActor ! "favoriteTech"
+      expectMsgPF() {
+        case "Scala" => // only care that the PF is defined
+        case "Akka" =>
+      }
+    }
+  }
+
+  // message assertions
+
+}
+
+object BasicSpec {
+
+  class SimpleActor extends Actor {
+    override def receive: Receive = {
+      case message => sender() ! message
+    }
+  }
+
+  class Blackhole extends Actor {
+    override def receive: Receive = Actor.emptyBehavior
+  }
+
+  class LabTestActor extends Actor {
+    val random = new Random()
+
+    override def receive: Receive = {
+      case "greeting" => if (random.nextBoolean()) sender() ! "hi" else sender() ! "hello"
+      case "favoriteTech" =>
+        sender() ! "Scala"
+        sender() ! "Akka"
+      case message: String => sender() ! message.toUpperCase()
+    }
+  }
+}
